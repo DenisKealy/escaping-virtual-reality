11f1dc4c86ca20cf4b336ef94fb34542bf83eeb8
Denis Kealy <deniskealyandroid@gmail.com>
2017-01-04 16:27:48 +0000

    Added 2 game levels. In the second level I added a falling star that the player can catch and interact with (StarSpawner/StarDeystoyer scripts & FallingStar prefab).

---
+++ Assets/Escaping VR/Scripts/StarSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Class used to spawn falling stars in Level 2
+public class StarSpawner : MonoBehaviour {
+
+	// Public members assigned in Unity Editor
+	public float timeBetweenStars;
+	public Rigidbody starPrefab;
+
+	// Countdown timer until next star is spawned
+	private float spawnTimer;
+
+
+	void Start () {
+	
+		spawnTimer = timeBetweenStars;
+
+	}
+
+	void Update () {
+
+		// Update timer
+		spawnTimer -= Time.deltaTime;
+
+		// If timer is less than zero
+		if(spawnTimer < 0) {
+			// Spawn a star & reset the timer
+			Instantiate(starPrefab, this.transform.position, this.transform.rotation);
+			spawnTimer = timeBetweenStars;
+		}
+
+	}
+
+}
