d851068dc00d84ff996245f2430c3d9d63b38958
Denis Kealy <deniskealyandroid@gmail.com>
2017-01-04 16:15:36 +0000

    Added Teleporter Audio cues & a null exception check to Teleport.cs. Removed old scenes.

--- Assets/Escaping VR/Scripts/Teleport.cs
+++ Assets/Escaping VR/Scripts/Teleport.cs
@@ -1,31 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Class used to provide GameObjects with the ability to pass through Teleporters
+
 public class Teleport : MonoBehaviour {
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 
 	// Triggered when the this component's GameObject hits a Collider that is a Trigger
 	void OnTriggerEnter(Collider col) {
 		// If the Collider's game object is tagged as a Teleporter
 		if (col.gameObject.tag == "Teleporter") {
 
-			// Retrieve the TeleportDestination object from the Collider
-			TeleportDestination destination = col.gameObject.GetComponent<TeleportDestination>();
+			// Retrieve the TeleportFunctionality object from the Collider
+			TeleportFunctionality teleporter = col.gameObject.GetComponent<TeleportFunctionality>();
 
-			// Transform to the position of the TeleportDestination's target and take it's rotation
-			this.transform.position = destination.target.transform.position;
-			this.transform.rotation = destination.target.transform.rotation;
+			// Make sure we don't get null exception
+			// Objects tagged as a teleporter should have TelportFunctionality script attached
+			if(teleporter){
+				if(teleporter.EndOfLevel) {
+					teleporter.LevelCompleted();
+				}
+				else {
+					
+					// Transform to the position of the TeleportDestination's target and take it's rotation
+					this.transform.position = teleporter.target.transform.position;
+					this.transform.rotation = teleporter.target.transform.rotation;
+				}
+
+				// Play the audio cue
+				teleporter.PlayTelportSound();
+			}
+
 		}
 
 	}
---
+++ Assets/Escaping VR/Scripts/TeleportFunctionality.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Class used to turn GameObjects into teleporters
+public class TeleportFunctionality : MonoBehaviour {
+
+	// Public member to be assigned in the Unity Editor
+	public GameObject target;
+
+	// Is this a teleporter to another level
+	public bool EndOfLevel = false;
+	
+
+	public void PlayTelportSound() {
+		AudioSource teleportSound = GetComponent<AudioSource>();
+		teleportSound.Play();
+	}
+
+	public void LevelCompleted() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+}
