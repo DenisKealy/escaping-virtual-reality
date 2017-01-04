c5b23a63887185a6d722fb53ed9b9fdf95e8ffa2
Denis Kealy <deniskealyandroid@gmail.com>
2017-01-04 16:23:30 +0000

    Added the HoloChamber consisting of 3 scenes. HoloChamberListener.cs listens for key presses on the keyboard which will swap between the different scenes. I couldn't find a way to do so in-game because of versioning & time constraints.

---
+++ Assets/Escaping VR/Scripts/HoloChamberListener.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoloChamberListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.M))
+		{
+			Application.LoadLevel("Holo Moon");
+		}
+		if(Input.GetKeyDown(KeyCode.K))
+		{
+			Application.LoadLevel("Holo Campfire");
+		}
+		if(Input.GetKeyDown(KeyCode.H))
+		{
+			Application.LoadLevel("Holo Chamber");
+		}
+	}
+}
